97b1bcdbac9315edef0437f0918a2f099411fbbd
MrNubOK <tibets1995@mail.ru>
2022-09-08 18:14:13 +0300

    merkle tree fix

--- contracts/MerkleTree.sol
+++ contracts/MerkleTree.sol
@@ -5,52 +5,58 @@ pragma solidity ^0.8.0;
 contract MerkleTree {
     bytes32[] public hashes;
     string[4] transactions = [
-        "TX: 1 => 2",
-        "TX: 2 => 3",
-        "TX: 3 => 1",
-        "TX: 2 => 1"
+        "TX1: 1 => 2",
+        "TX1: 2 => 3",
+        "TX1: 3 => 1",
+        "TX1: 1 => 4"
     ];
+
+    //3: 
+    //5:
+    //root:  0xf1acc62dd99a3821dba5aecf234aaf0457a931b07da7c4c0117af8e2be0ade58
 
     constructor() {
         uint count = transactions.length;
-
         for (uint i = 0; i < count; i++) {
-            hashes.push(makeHash(transactions[i]));
+            hashes.push(
+                keccak256(
+                    abi.encodePacked(transactions[i])
+                )
+            );
         }
 
         uint offset = 0;
         while (count > 0) {
             for (uint i = 0; i < count - 1; i += 2) {
-                hashes.push(keccak256(abi.encodePacked(hashes[offset + i], hashes[offset + i + 1])));
+                hashes.push(
+                    keccak256(
+                        abi.encodePacked(hashes[offset + i], hashes[offset + i + 1])
+                    )
+                );
             }
 
-            count = count / 2;
             offset += count;
+            count = count / 2;
         }
     }
 
     function verify(string memory transaction, uint index, bytes32 root, bytes32[] memory proof) public pure returns(bool) {
-        bytes32 transactionHash = makeHash(transaction);
-        for (uint i = 0; i < proof.length - 1; i++) {
+        bytes32 hash = keccak256(abi.encodePacked(transaction));
+
+        //0 1 2 3 
+        //
+        for (uint i = 0; i < proof.length; i++) {
             bytes32 element = proof[i];
 
             if (index % 2 == 0) {
-                transactionHash = keccak256(abi.encodePacked(transactionHash, element));
+                hash = keccak256(abi.encodePacked(hash, element));
             } else {
-                transactionHash = keccak256(abi.encodePacked(element, transactionHash));
+                hash = keccak256(abi.encodePacked(element, hash));
             }
 
             index = index / 2;
         }
 
-        return transactionHash == root;
-    }
-
-    function encode(string memory input) public pure returns(bytes memory) {
-        return abi.encodePacked(input);
-    }
-
-    function makeHash(string memory input) public pure returns(bytes32) {
-        return keccak256(encode(input));
-    }
+        return hash == root;
+    }   
 }
