4f57fa80fdcd4463c3d8c33ed424ff4cbab172a3
MrNubOK <vadzim.shadryn@gmail.com>
2022-09-19 22:18:54 +0300

    smart with frontend

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -4,23 +4,63 @@
 // You can also run a script with `npx hardhat run <script>`. If you do that, Hardhat
 // will compile your contracts, add the Hardhat Runtime Environment's members to the
 // global scope, and execute the script.
+const { ethers } = require("hardhat");
 const hre = require("hardhat");
+const fs = require("fs")
+const path = require("path")
 
 async function main() {
-  const currentTimestampInSeconds = Math.round(Date.now() / 1000);
-  const ONE_YEAR_IN_SECS = 365 * 24 * 60 * 60;
-  const unlockTime = currentTimestampInSeconds + ONE_YEAR_IN_SECS;
+  if (network.name === "hardhat") {
+    console.warn("You use Hardhat network");
+  }
 
-  const lockedAmount = hre.ethers.utils.parseEther("1");
+  // const ONE_DAY_IN_SECS = 365 * 24 * 60 * 60;
 
-  const Lock = await hre.ethers.getContractFactory("Lock");
-  const lock = await Lock.deploy(unlockTime, { value: lockedAmount });
+  const [deployer] = await ethers.getSigners()
+  console.log("Deploying with " + await deployer.getAddress())
 
-  await lock.deployed();
+  const Auction = await ethers.getContractFactory("DutchAuction", deployer)
+  const auction = await Auction.deploy(
+    ethers.utils.parseEther("2.0"),
+    100,
+    "Test item"
+  )
+  
+  await auction.deployed();
+
+  saveContractFiles({
+    DutchAuction: auction
+  })
 
   console.log(
-    `Lock with 1 ETH and unlock timestamp ${unlockTime} deployed to ${lock.address}`
+    `Contract was successfully deployed`
   );
+}
+
+function saveContractFiles(contracts) {
+  const contractsDir = path.join(__dirname, '..', 'front/contracts')
+
+  if (!fs.existsSync(contractsDir)) {
+    fs.mkdirSync(contractsDir)
+  }
+
+  Object.entries(contracts).forEach(item => {
+    const [name, contract] = item
+
+    if (contract) {
+      fs.writeFileSync(
+        path.join(contractsDir, '/', name + '-contracts-address.json'),
+        JSON.stringify({[name]: contract.address}, undefined, 2)
+      )
+    }
+
+    const ContractArtifact = hre.artifacts.readArtifactSync(name) 
+
+    fs.writeFileSync(
+      path.join(contractsDir, '/', name + '.json'),
+      JSON.stringify(ContractArtifact, null, 2)
+    )
+  })
 }
 
 // We recommend this pattern to be able to use async/await everywhere
